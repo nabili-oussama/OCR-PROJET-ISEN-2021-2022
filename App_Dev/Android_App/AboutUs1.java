8ad3439cfe0b1c8205e9940c58899ca7647273e9
nabili-oussama <75122232+nabili-oussama@users.noreply.github.com>
2022-06-29 23:52:20 +0100

    Add files via upload

---
+++ App_Dev/Android_App/AboutUs1.java
@@ -0,0 +1,17 @@
+package com.ocr1.ocrapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class AboutUs1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us1);
+        getSupportActionBar().hide();
+    }
+}
---
+++ App_Dev/Android_App/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ocr1.ocrapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+                                      @Override
+                                      public void run() {
+                                          startActivity(new Intent(MainActivity.this, Home.class));
+                                          finish();
+                                      }
+                                  }
+                , 2500);
+    }
+
+
+}
